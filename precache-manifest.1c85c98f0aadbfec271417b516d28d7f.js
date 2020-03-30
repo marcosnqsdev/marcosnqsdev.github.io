38e7c70cff36a028c9c57412798329bcc12484fc
marcosnqsdev <marcosnqsdev@gmail.com>
2020-03-30 18:08:50 -0300

    Updates

---
+++ precache-manifest.1c85c98f0aadbfec271417b516d28d7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db808defe500b157c5c1fc9d76247f3",
+    "url": "/marcosnqsdev.github.io/index.html"
+  },
+  {
+    "revision": "f9fe43a84afe1e3b4fa3",
+    "url": "/marcosnqsdev.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "454339996ab46a48387b",
+    "url": "/marcosnqsdev.github.io/static/js/2.a607b981.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marcosnqsdev.github.io/static/js/2.a607b981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9fe43a84afe1e3b4fa3",
+    "url": "/marcosnqsdev.github.io/static/js/main.e0eb4806.chunk.js"
+  },
+  {
+    "revision": "375fb077f7b5222cfcff",
+    "url": "/marcosnqsdev.github.io/static/js/runtime-main.abc0a885.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marcosnqsdev.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
