bb2ac35454dd3b16c7317dc8a915d5e27254b5f8
marcosnqsdev <marcosnqsdev@gmail.com>
2020-03-30 18:48:52 -0300

    React page master

---
+++ precache-manifest.7985dfa97ef06e343b29a13911af95d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0100fef89c7003b23ab04a970e49d2e",
+    "url": "/marcosnqsdev.github.io/index.html"
+  },
+  {
+    "revision": "b055575e399662a1b955",
+    "url": "/marcosnqsdev.github.io/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "454339996ab46a48387b",
+    "url": "/marcosnqsdev.github.io/static/js/2.a607b981.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marcosnqsdev.github.io/static/js/2.a607b981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b055575e399662a1b955",
+    "url": "/marcosnqsdev.github.io/static/js/main.9b057fcf.chunk.js"
+  },
+  {
+    "revision": "375fb077f7b5222cfcff",
+    "url": "/marcosnqsdev.github.io/static/js/runtime-main.abc0a885.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/marcosnqsdev.github.io/static/media/logo.ee7cd8ed.svg"
+  }
+]);
